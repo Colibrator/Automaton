9e35c95b0760fd59faa291b51586d5464f1a571a
Colibrator <timtim40a@gmail.com>
2022-12-09 22:28:35 +0000

    Initial upload of a program

---
+++ rules.py
@@ -0,0 +1,25 @@
+def rules(x, y, button_list, col1="black", col2="white") -> int:
+    for i in range(x):
+        for j in range(y):
+            adjace_life = 0
+            if i > 0 and button_list[i - 1][j]["bg"] == col2:
+                adjace_life += 1
+            if i > 0 and j < y - 1 and button_list[i - 1][j + 1]["bg"] == col2:
+                adjace_life += 1
+            if j < y - 1 and button_list[i][j + 1]["bg"] == col2:
+                adjace_life += 1
+            if i < x - 1 and j < y - 1 and button_list[i + 1][j + 1]["bg"] == col2:
+                adjace_life += 1
+            if i < x - 1 and button_list[i + 1][j]["bg"] == col2:
+                adjace_life += 1
+            if i < x - 1 and j > 0 and button_list[i + 1][j - 1]["bg"] == col2:
+                adjace_life += 1
+            if j > 0 and button_list[i][j - 1]["bg"] == col2:
+                adjace_life += 1
+            if i > 0 and j > 0 and button_list[i - 1][j - 1]["bg"] == col2:
+                adjace_life += 1
+            if (adjace_life < 2 or adjace_life > 3) and button_list[i][j]["bg"] == col2:
+                button_list[i][j]["fg"] = col1
+            elif adjace_life == 3 and button_list[i][j]["bg"] == col1:
+                button_list[i][j]["fg"] = col2
+    return adjace_life
---
+++ secondary.py
@@ -0,0 +1,12 @@
+"""from tkinter import *
+from main import dimensions_change
+
+
+entr_color1 = Entry(branch)
+    entr_color2 = Entry(branch)
+but_color1 = Button(branch, text="Change passive color")
+    but_color2 = Button(branch, text="Change active color")
+lab_color1 = Label(branch, text="Enter the HEX code of desired passive color")
+    lab_color2 = Label(branch, text="Enter the HEX code of desired active color")
+
+"""
